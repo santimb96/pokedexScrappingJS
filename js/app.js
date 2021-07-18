de746a3591a9f4e7cc74a262e0494e35048df52d
Santiago <Martnez>
2021-07-18 03:43:41 +0200

    longitud datos.results actualizada en bucle for

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const app = {
      */
     mostrarDatos: function (datos) {
         let pokemons = ''; //guardará el renderizado del for
-        for (let i = 0; i < 385; i++) { //evalua cada pokémon en la dirección datos.results[índice del pokémon].name
+        for (let i = 0; i < datos.results.length; i++) { //evalua cada pokémon en la dirección datos.results[índice del pokémon].name
             //cada pokémon es mandado a la variable con el siguiente formato de renderizado
             pokemons+=`<tr class="active-row"><td>${i+1}</td><td>${datos.results[i].name.charAt(0).toUpperCase()+datos.results[i].name.slice(1)}</td></tr>`;
         }
